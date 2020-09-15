dab0993ab608460a6d0d80f75d08ce8a3183b950
lpagn <44617289+lpagn@users.noreply.github.com>
2020-09-15 12:46:12 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -17,3 +17,11 @@ chmod u+x ./POD-server-1.0-SNAPSHOT/run-server.sh
 cd $CODEBASE/client/target/
 
 tar xzf POD-client-1.0-SNAPSHOT-bin.tar.gz
+
+chmod u+x ./POD-server-1.0-SNAPSHOT/run-management.sh
+
+chmod u+x ./POD-server-1.0-SNAPSHOT/run-query.sh
+
+chmod u+x ./POD-server-1.0-SNAPSHOT/run-fiscal.sh
+
+chmod u+x ./POD-server-1.0-SNAPSHOT/run-vote.sh
