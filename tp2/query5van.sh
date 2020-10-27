8b7f75660029c3920551470177b39cfde55a2789
lpagn <44617289+lpagn@users.noreply.github.com>
2020-10-27 15:56:02 -0300

    Update query5van.sh

--- tp2/query5van.sh
+++ tp2/query5van.sh
@@ -1,1 +1,4 @@
-./query5 -Dcity=VAN -Daddresses='10.6.0.1:5701' -DinPath=/Users/luciopagni/Desktop/pod-tpe-2/client/src/main/resources/ -DoutPath=/Users/luciopagni/Desktop/pod-tpe-2/results/
+OUTPATH="/home/lpagni/Escritorio/pod-tpe-2/results/"
+
+./query5 -Dcity=VAN -Daddresses='10.6.0.1:5701' -DinPath=/afs/it.itba.edu.ar/pub/pod/ -DoutPath
+=$OUTPATH
